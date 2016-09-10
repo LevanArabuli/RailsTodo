3f469a5970d19df999f70b750805b739bfa7056c
Levan Arabuli <larabuli@uplandsoftware.com>
2016-09-10 22:21:21 +0400

    Add todo controller

---
+++ db/migrate/20160910181815_create_todos.rb
@@ -0,0 +1,12 @@
+class CreateTodos < ActiveRecord::Migration[5.0]
+  def change
+    create_table :todos do |t|
+      t.string :title
+      t.text :description
+      t.datetime :due_date
+      t.boolean :is_done
+
+      t.timestamps
+    end
+  end
+end
