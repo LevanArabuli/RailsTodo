d97e7d29587834006e1f92a641c2ce5daa2f52c7
Levan Arabuli <larabuli@uplandsoftware.com>
2016-09-11 00:17:38 +0400

    Add checkboxes to index page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20160910181815) do
-
   create_table "todos", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
@@ -20,5 +19,4 @@ ActiveRecord::Schema.define(version: 20160910181815) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
-
 end
