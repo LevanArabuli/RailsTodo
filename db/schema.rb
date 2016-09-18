02c70db1c15bcaef19ed76b5968b11d2e0b297d9
Levan Arabuli <larabuli@uplandsoftware.com>
2016-09-18 14:45:31 +0400

    Add todos to users FK

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160918102514) do
+ActiveRecord::Schema.define(version: 20160918103956) do
 
   create_table "todos", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20160918102514) do
     t.boolean  "is_done"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_todos_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
